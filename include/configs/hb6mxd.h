5e7ddb4f5372504f855317e3eb66e918905b0663
Frodo Lai <frodo_lai@bcmcom.com>
2013-07-26 10:11:52 -0700

    Fix memory size and boot parameter for HB6MXD
    Set memory size to 1024MB.
    Remove "nosmp" from boot parameter.

--- include/configs/hb6mxd.h
+++ include/configs/hb6mxd.h
@@ -160,7 +160,7 @@
 		"kernel=uImage\0"				\
 		"nfsroot=/opt/eldk/arm\0"				\
 		"bootargs_base=setenv bootargs console=ttymxc3,115200 "\
-		"vmalloc=400M consoleblank=0 nosmp arm_freq=1000\0"     \
+		"vmalloc=400M consoleblank=0 arm_freq=1000\0"     \
 		"bootargs_nfs=setenv bootargs ${bootargs} root=/dev/nfs "\
 			"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0"\
 		"bootcmd_net=run bootargs_base bootargs_nfs; "		\
